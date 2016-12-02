736f95cef909a271dbf7f34f78a4676e588872e5
sheny <sheny@ngarihealth.com>
2016-12-02 16:07:27 +0800

    1234

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by shenyang on 2016/12/2.
+//  Copyright © 2016年 shenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by shenyang on 2016/12/2.
+//  Copyright © 2016年 shenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
